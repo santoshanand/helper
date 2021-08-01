3fefde3abe3268e8837c9bb1e56d308fed38466c
Santosh Anand <santoshanand.jogi@gmail.com>
2021-08-01 12:32:39 +0530

    updated refrences

--- helper.go
+++ helper.go
@@ -5,10 +5,7 @@ import (
 
 	"github.com/santoshanand/helper/config"
 	"github.com/santoshanand/helper/logger"
-	"go.uber.org/zap"
 )
-
-var zlog *zap.SugaredLogger
 
 // Initialize - initialie helper it will load config and initialize logger
 func Initialize() error {
@@ -16,7 +13,7 @@ func Initialize() error {
 		fmt.Println("configuration initialization error")
 		return err
 	}
-	zlog = logger.InitLogger(false)
+	logger.InitLogger(false)
 	return nil
 }
 
@@ -30,10 +27,10 @@ func GetConfigValue(key string) string {
 
 // Log - print log info on console.
 func Log(args ...interface{}) {
-	zlog.Info(args...)
+	logger.Info(args...)
 }
 
 // Logf - print formatted log info on console.
 func Logf(template string, args ...interface{}) {
-	zlog.Infof(template, args...)
+	logger.Infof(template, args...)
 }
